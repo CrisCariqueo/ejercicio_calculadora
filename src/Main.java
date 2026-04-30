5ac13375da574c53c133ce1ef823a2121fbd86ca
Cristobal Cariqueo <c.cariqueo03@ufromail.cl>
2026-04-30 16:41:58 -0400

    feat(calculadora): Implementar menú y validaciones de division por 0 y opcion invalida

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         int opcion = 0;
         int a = 0;
@@ -7,22 +10,47 @@ public class Main {
         // Base para el ejercicio: este switch debe ser trabajado en varias feature/*.
         // El conflicto intencional se genera cuando dos o mas features editan las
         // mismas lineas.
-        switch (opcion) {
-            case 1:
-                System.out.println("=" + suma(a, b));
-            case 2:
-                System.out.println("=" + resta(a, b));
-            case 3:
-                System.out.println(a * b);
-                break;
-            case 4:
-                System.out.println((float) a / b);
-                break;
-            default:
-                System.out.println("TODO: implementar menu y validaciones");
-                break;
-        }
+        while (true) {
+            switch (opcion) {
+                case 1:
+                    opcion = 0;
+                    System.out.println("=" + suma(a, b));
+                    break;
+                case 2:
+                    opcion = 0;
+                    System.out.println("=" + resta(a, b));
+                    break;
+                case 3:
+                    opcion = 0;
+                    System.out.println(a * b);
+                    break;
+                case 4:
+                    opcion = 0;
+                    System.out.println((float) a / b);
+                    break;
+                default:
+                    System.out.println("1:+  2:-  3:*  4:/");
+                    System.out.print("Elige una opcion: ");
+                    opcion = sc.nextInt();
+                    System.out.print("valor de a: ");
+                    a =  sc.nextInt();
+                    System.out.print("valor de b: ");
+                    b = sc.nextInt();
 
+                    if (opcion < 1 || opcion > 4) {
+                        System.out.println("Opcion invalida");
+                        opcion = 0;
+                        a = 0;
+                        b = 0;
+                    }
+                    if (opcion == 4 && b == 0) {
+                        System.out.println("No se puede dividir por 0");
+                        opcion = 0;
+                        a = 0;
+                        b = 0;
+                    }
+            }
+        }
     }
 
     public static int suma(int a, int b) {
